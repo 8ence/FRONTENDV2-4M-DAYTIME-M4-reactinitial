6a6dff1ea72641aa2aca1fecdf8d09be844c852b
Kővári Bencem <kovaribence95@gmail.com>
2022-10-19 17:31:34 +0200

    tryhard with input

--- reactinitial/src/App.js
+++ reactinitial/src/App.js
@@ -19,6 +19,7 @@ const App = () => {
 
     function FilterLaptops(evt) {
         setInput(evt.target.value)
+        laptops.filter((laptop) => laptop.brand.includes({ input }))
     }
 
     return (
